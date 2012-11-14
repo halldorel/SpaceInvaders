a8102f2f78894ac19f544465eb028e890975d98b
kreldjarn <kristjan@eldjarn.net>
2012-11-14 10:25:16 +0000

    Revert "Nokkrir klasar"
    This reverts commit d4b9e81629cf87f418eca54b125113499b54aa32.

--- kristjan/Collider.java
+++
@@ -1,9 +0,0 @@
-public abstract class Collider
-{
-	public boolean checkCollision(Collider that) {
-		Rectangle r = this.getBounds();
-		return r.intersects(that.getBounds());
-	}
-	
-	public abstract Rectangle getBounds()
-}
