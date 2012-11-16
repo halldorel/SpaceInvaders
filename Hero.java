b883bf04e14fb030d19be37bdb07bfd7fa9a231c
Halldór Eldjárn <halldor@eldjarn.net>
2012-11-16 12:07:08 +0000

    Hero.java

--- Hero.java
+++ Hero.java
@@ -16,19 +16,26 @@ public class Hero
 	private static double ypos = 0;
 	
 	// Leyfilegt svæði hetju
-	private static final double MIN_X = 0	+5;
-	private static final double MAX_X = 512	-5;
+	private static final double MIN_X = 0;
+	private static final double MAX_X = 512;
 	
 	// Lyklar fyrir vinstri-hægri hreyfingar hetju
 	private static final int LEFT = KeyEvent.VK_A;
 	private static final int RIGHT = KeyEvent.VK_D;
+	
+	// Stærð hetju
+	private static final int WIDTH = 30;
+	private static final int HEIGHT = 10;
+	
+	// Er hetja lifandi?
+	private static boolean alive = true;
 	
 	// Constructor
 	public Hero(double xpos)
 	{
 		this.xpos = xpos;
 		// ATH hér eru einhverjar temp stærðir á hetju (30, 10))
-		this.hero = new Rectangle(xpos, ypos, 30, 10);
+		this.bounds = new Rectangle(xpos, ypos, 30, 10);
 	}
 	
 	public double getX()
@@ -41,25 +48,43 @@ public class Hero
 		// Bregðast við 
 		if (StdDraw.isKeyPressed(LEFT))
 			{
-				if (this.xpos > MIN_X)
+				// GAME FRAME - breyta skv Game.java
+				if (this.xpos > MIN_X + WIDTH/2)
 				{
-					hero.setX(hero.getX() - SPEED);
+					bounds.setX(bounds.getX() - SPEED);
 				}
 			}
 		else if (StdDraw.isKeyPressed(RIGHT))
 			{
-				if (this.xpos < MAX_X)
+				// GAME FRAME
+				if (this.xpos < MAX_X - WIDTH/2)
 				{
-					hero.setX(hero.getX() + SPEED);
+					bounds.setX(bounds.getX() + SPEED);
 				}
 			}
 	}
 	
+	// Er leikmaður lifandi?
+	public boolean isAlive()
+	{
+		return alive;
+	}
+	
+	public void kill()
+	{
+		this.alive = false;
+	}
+
 	public void draw()
 		{
 			// Rectangle draw
 			hero.show();
 		}
+
+	public boolean collides(Rectangle r)
+	{
+		return this.bounds.intersects(r);
+	}
 
 	// Test client
 	public static void main(String[] args)
